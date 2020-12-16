f794bf15a2578fce3995afe1048adefa5b8802a4
Vitalii <movcan77@gmail.com>
2020-12-16 14:57:20 +0200

    Hide Check implementation details

--- src/week2/day2/pointsSilpo/checkout/Check.java
+++ src/week2/day2/pointsSilpo/checkout/Check.java
@@ -1,14 +1,21 @@
 package week2.day2.pointsSilpo.checkout;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Check {
-
-    public List<Product> products;
-    public int totalCost;
+    private List<Product> products = new ArrayList<>();
 
     public int getTotalCost() {
+        int totalCost = 0;
+        for (Product product : this.products) {
+            totalCost += product.price;
+        }
         return totalCost;
+    }
+
+    void addProduct(Product product) {
+        products.add(product);
     }
 
 }
