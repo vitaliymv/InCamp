dc31e68cab9c3972331e829d963b7e64e9341fe8
Vitalii <movcan77@gmail.com>
2020-12-16 14:25:40 +0200

    Rename class

--- src/week2/day1/groupbirthday/MainApplication.java
+++ src/week2/day1/groupbirthday/MainApplication.java
@@ -1,14 +1,14 @@
 package week2.day1.groupbirthday;
 
 
-import week2.day1.groupbirthday.service.GroupDataByMonth;
+import week2.day1.groupbirthday.service.GroupDataByMonthService;
 
 public class MainApplication {
 
     public static void main(String[] args) {
 
-        GroupDataByMonth groupDataByMonth = new GroupDataByMonth();
-        groupDataByMonth.getGroupingEmployee();
+        GroupDataByMonthService groupDataByMonthService = new GroupDataByMonthService();
+        groupDataByMonthService.getGroupingEmployee();
 
     }
 
