7eeefb0e758383391ddcb52dc80dda0c1c8b4893
Vitalii <movcan77@gmail.com>
2020-12-16 14:24:39 +0200

    Main method

---
+++ src/week2/day1/groupbirthday/MainApplication.java
@@ -0,0 +1,15 @@
+package week2.day1.groupbirthday;
+
+
+import week2.day1.groupbirthday.service.GroupDataByMonth;
+
+public class MainApplication {
+
+    public static void main(String[] args) {
+
+        GroupDataByMonth groupDataByMonth = new GroupDataByMonth();
+        groupDataByMonth.getGroupingEmployee();
+
+    }
+
+}
