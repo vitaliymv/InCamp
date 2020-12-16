43a86d4803f87fbddcf5cf8574bbe87f804c5f6a
Vitalii <movcan77@gmail.com>
2020-12-16 15:16:59 +0200

    Reopen check if it closed and calculate default points

--- src/week2/day2/pointsSilpo/checkout/Check.java
+++ src/week2/day2/pointsSilpo/checkout/Check.java
@@ -18,4 +18,8 @@ public class Check {
         products.add(product);
     }
 
+    public int getTotalPoints() {
+        return getTotalCost();
+    }
+
 }
--- src/week2/day2/pointsSilpo/test/CheckoutServiceTest.java
+++ src/week2/day2/pointsSilpo/test/CheckoutServiceTest.java
@@ -1,5 +1,6 @@
 package week2.day2.pointsSilpo.test;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import week2.day2.pointsSilpo.checkout.Check;
 import week2.day2.pointsSilpo.checkout.CheckoutService;
@@ -12,12 +13,23 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CheckoutServiceTest {
 
-    @Test
-    void closeCheck__withOneProduct() {
-        CheckoutService checkoutService = new CheckoutService();
+    private Product milk_7;
+    private CheckoutService checkoutService;
+    private Product bred_3;
+
+
+    @BeforeEach
+    void setUp() {
+        checkoutService = new CheckoutService();
         checkoutService.openCheck();
 
-        checkoutService.addProduct(new Product(7, "Milk"));
+        milk_7 = new Product(7, "milk");
+        bred_3 = new Product(3, "bred");
+    }
+
+    @Test
+    void closeCheck__withOneProduct() {
+        checkoutService.addProduct(milk_7);
         Check check = checkoutService.closeCheck();
 
         assertThat(check.getTotalCost(), is(7));
@@ -25,14 +37,31 @@ public class CheckoutServiceTest {
 
     @Test
     void closeCheck__withTwoProducts() {
-        CheckoutService checkoutService = new CheckoutService();
-        checkoutService.openCheck();
-
-        checkoutService.addProduct(new Product(7, "Milk"));
-        checkoutService.addProduct(new Product(3, "Bred"));
+        checkoutService.addProduct(milk_7);
+        checkoutService.addProduct(bred_3);
         Check check = checkoutService.closeCheck();
 
         assertThat(check.getTotalCost(), is(10));
+    }
+
+    @Test
+    void addProduct__whenCheckIsClosed__opensNewCheck() {
+        checkoutService.addProduct(milk_7);
+        Check milkCheck = checkoutService.closeCheck();
+        assertThat(milkCheck.getTotalCost(), is(7));
+
+        checkoutService.addProduct(bred_3);
+        Check bredCheck = checkoutService.closeCheck();
+        assertThat(bredCheck.getTotalCost(), is(3));
+    }
+
+    @Test
+    void closeCheck__calcTotalPoints() {
+        checkoutService.addProduct(milk_7);
+        checkoutService.addProduct(bred_3);
+        Check check = checkoutService.closeCheck();
+
+        assertThat(check.getTotalPoints(), is(10));
     }
 
 }
